fd52c1c3b1e610f2a7a7b96020a24d83f459933b
SetropLeo <leol_portes@hotmail.com>
2024-12-15 09:39:21 -0300

    Adjusts login

--- src/actions/loginAction.ts
+++ src/actions/loginAction.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { NextResponse } from "next/server";
+
 export const loginAction = async (username: string, password: string) => {
   const MOCK_USER = {
     username: "testuser",
@@ -7,8 +9,18 @@ export const loginAction = async (username: string, password: string) => {
   };
 
   if (username === MOCK_USER.username && password === MOCK_USER.password) {
-    return true;
-  } else {
-    return false;
+    return NextResponse.json(
+      { success: true, token: generateToken() },
+      { status: 200 }
+    );
   }
+
+  return NextResponse.json(
+    { success: false, message: "Invalid credentials" },
+    { status: 401 }
+  );
+};
+
+const generateToken = () => {
+  return Math.floor(Math.random() * (9999999999 - 1000000000 + 1)) + 1000000000;
 };
